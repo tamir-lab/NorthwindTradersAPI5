b22f0cefa88936866c8141ede33197743dae1d90
tamir <tamchik00@gmail.com>
2025-12-10 15:02:24 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI5/dao/interfaces/IProductDao.java
@@ -0,0 +1,16 @@
+package com.pluralsight.NorthwindTradersAPI5.dao.interfaces;
+
+
+
+import com.pluralsight.NorthwindTradersAPI5.models.Category;
+import com.pluralsight.NorthwindTradersAPI5.models.Product;
+
+import java.util.List;
+
+public interface IProductDao {
+    List<Product> getAll();
+    Product getById(int id);
+
+    Product insert(Product product);
+    void update(int productId, Product product);
+}
